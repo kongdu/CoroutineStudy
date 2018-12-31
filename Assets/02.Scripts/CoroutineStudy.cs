5188003837ee1ce952b85e68552253ba074395aa
yeon <yeonyya@gmail.com>
2018-12-31 16:59:12 +0900

    first commit

---
+++ Assets/02.Scripts/CoroutineStudy.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//프로그램의 흐름을 중단하고, 유니티에 제어권을 돌려주고 다음에 다시 중지한곳부터 실행을 계속할 수 있는 기능
+//특정 작업을 단계적으로 실행하거나, 시간이 흐름에 따라 발생하는 코드를 작성하거나, 다른 연산이 완료될 때까지 기다리는 등의 코드를 작성할 수 있음
+
+public class CoroutineStudy : MonoBehaviour
+{
+    public float duration = 5.0f;
+    public Vector3 beginPos;
+    public Vector3 endPos;
+    public Vector3 distance;
+
+    public AnimationCurve curve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+
+    void Awake()
+    {
+        distance = endPos - beginPos;
+        //StartCoroutine(ShowTime());
+        //StartCoroutine(AnimalSound());
+        StartCoroutine(CountDown(duration));
+    }
+
+    //카운트다운기능
+    IEnumerator CountDown(float duration)
+    {
+        var beginTime = Time.time;
+        var percent = 0f;
+        for (; ; )
+        {
+
+            percent = (Time.time - beginTime)/duration;
+            if (percent > 1f)
+
+            {
+                break;
+            }
+                //Debug.Log(time);
+                //transform.position = beginPos + distance * percent;
+                transform.position = beginPos + distance * curve.Evaluate(percent);
+             
+            //} else
+            //{
+            //    Debug.Log("0.00");
+            //    break;
+            //}
+
+            yield return null;
+        }
+        transform.position = endPos;
+        Debug.Log("시작!!!");
+    }
+
+    IEnumerator ShowTime()
+    {
+        yield return new WaitForSeconds(1f);
+        Debug.Log(System.DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss"));
+
+    }
+
+
+    IEnumerator AnimalSound()
+    {
+        yield return new WaitForSeconds(1f);
+        Debug.Log("야옹");
+        yield return new WaitForSeconds(3f);
+        Debug.Log("어흥");
+        for (; ; )
+        {
+            yield return new WaitForSeconds(.5f);
+            Debug.Log("yap");
+        }
+    }
+
+}
---
+++ Assets/02.Scripts/RSPGame.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Mode { Rock, Scissors, Paper };
+
+public enum Result { Win, Lose, Same };
+
+public class RSPGame : MonoBehaviour
+{
+    public float waitingTme = 5f;
+    public Mode MyRsp;
+    public Result res;
+
+    private IEnumerator Start()
+    {
+        //1. 카운트다운 5초
+        yield return StartCoroutine(CountDown(waitingTme));
+        //1. NPC의 패 선택
+        Mode comSrp = GetComSRP();
+        //1.5 이와중에 내 패 Inspector 창에서 선택해야함
+        //2. 승패판단
+        Result result = PlayGame(comSrp, MyRsp);
+        //3. 승패 출력
+        showResult(result, comSrp, MyRsp);
+        //4. 반복
+        //포문 추가
+    }
+
+    public Mode GetComSRP()
+    {
+        Mode ComSelet = (Mode)Random.Range(0, 3); //0이상 3미만 0, 1, 2
+
+        Debug.Log("컴퓨터 : " + ComSelet);
+        return ComSelet; //컴퓨터가 낸 패를 리턴
+    }
+
+    public Result PlayGame(Mode com, Mode me)
+    {
+        Debug.Log("*****RSP Game Start*****");
+
+        if (com == me)
+        {
+            return Result.Same;
+        }
+        switch (MyRsp)
+        {
+            case Mode.Rock: //내가 묵
+                if (Mode.Paper == com)
+                {
+                    return Result.Lose;
+                }
+                else if (Mode.Scissors == com)
+                {
+                    return Result.Win;
+                }
+                break;
+
+            case Mode.Scissors: //내가 가위
+                if (Mode.Rock == com)
+                {
+                    return Result.Lose;
+                }
+                else if (Mode.Paper == com)
+                {
+                    return Result.Win;
+                }
+                break;
+
+            case Mode.Paper: //내가 보
+                if (Mode.Scissors == com)
+                {
+                    return Result.Lose;
+                }
+                else if (Mode.Rock == com)
+                {
+                    return Result.Win;
+                }
+                break;
+        }
+        return Result.Same;
+    }
+
+    public void showResult(Result result, Mode com, Mode me)
+    {
+        Debug.Log("당신은 " + result + "");
+        Debug.Log("나: " + me + "컴: " + com);
+    }
+
+    private IEnumerator CountDown(float wTime)
+    {
+        var beginTime = Time.time;
+        var time = 0f;
+
+        for (; ; )
+        {
+            time = waitingTme - (Time.time - beginTime);
+            if (time >= 0)
+            {
+                Debug.Log(time.ToString("0.00"));
+            }
+            else
+            {
+                Debug.Log("0");
+                break;
+            }
+
+            yield return null;
+        }
+        Debug.Log("시작!!");
+    }
+}
